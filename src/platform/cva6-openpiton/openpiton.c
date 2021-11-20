ad88ecb83be6f1129264d8e76f45ed22ec7b9fd2
ninolomata <bruno.vilaca.sa@gmail.com>
2021-11-20 01:07:33 +0000

    add support for cva6 openpiton

---
+++ src/platform/cva6-openpiton/openpiton.c
@@ -0,0 +1,33 @@
+#include <8250_uart.h>
+
+#define OPENPITON_DEFAULT_UART_ADDR		    0xfff0c2c000
+#define OPENPITON_DEFAULT_UART_FREQ		    60000000
+#define OPENPITON_DEFAULT_UART_BAUDRATE		115200
+#define OPENPITON_DEFAULT_UART_REG_SHIFT	0
+#define OPENPITON_DEFAULT_UART_REG_WIDTH	1
+
+
+void uart_init(){
+    uart8250_init(OPENPITON_DEFAULT_UART_ADDR, OPENPITON_DEFAULT_UART_REG_SHIFT,
+			     OPENPITON_DEFAULT_UART_BAUDRATE, 0, 1);
+}
+
+void uart_putc(char c)
+{
+    uart8250_putc(c);
+}
+
+char uart_getchar(void)
+{
+    return uart8250_getc();
+}
+
+void uart_enable_rxirq()
+{
+    uart8250_enable_rx_int();
+}
+
+void uart_clear_rxirq()
+{
+    uart8250_interrupt_handler(); 
+}
